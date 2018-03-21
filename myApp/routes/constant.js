a3ec2869679b4fd2cd8df4767f1e6930238fe263
Huy <huyph.mobile@gmail.com>
2018-03-21 11:52:18 +0700

    no message

---
+++ myApp/routes/constant.js
@@ -0,0 +1,4 @@
+const dbUrl = 'mongodb://11.0.4.189:27017/';
+module.exports = {
+  dbUrl
+}
--- myApp/routes/good.js
+++ myApp/routes/good.js
@@ -1,27 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://11.0.4.189:27017/";
+
+const {dbUrl} = require('./constant');
+
 /* GET goods listing. */
 router.get('/', function(req, res, next) {
 
-    MongoClient.connect(url, function(err, db) {
+  res.send('hello');
+
+});
+
+router.get('/getAll', function(req, res, next) {
+
+    MongoClient.connect(dbUrl, function(err, db) {
       if (err) throw err;
       console.log("Database created!");
-      var dbo = db.db("local");
-      dbo.collection("startup_log").find().toArray(function(err, result) {
-      if (err) throw err;
-      console.log(result);
-      res.send(result);
+      var dbo = db.db("shoes_db");
 
+      dbo.collection("products").find().toArray(function(err, result) {
+      if (err) throw err;
+
+      res.send(result);
+    
       db.close();
       });
 
     });
 
 });
-
-
 
 
 
